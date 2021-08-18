871d3d7ac830c946a89b2a40441b0cdd1e2fc321
Pavel <88209234+ProtocolPav@users.noreply.github.com>
2021-08-18 11:52:11 +0300

    Put all activity commands into a Cog
    The cog is called Activity, and it helps to separate all of the code. It also makes it usable cross-programs. I can have 1 file for activity, 1 file for something else. It helps the bot be more clean and organized

--- thorny.py
+++ thorny.py
@@ -12,12 +12,90 @@ screams = ['AAAaaaAaaaAAaaaAAAAAaAAaAAAAAaaAA', 'ARGHHHHHHHHHHHHHHHHHHHHhhhhhhhh
            '*screams*',
            'https://tenor.com/view/scream-yell-mad-angry-fury-gif-3864070']
 
+
 @client.event
 async def on_ready():
     print(f"Logged in as {client.user}")
     botactivity = discord.Activity(type=discord.ActivityType.playing,
                                    name="on Everthorn. Mining away. I don't know, what to mine!")
     await client.change_presence(activity=botactivity)
+
+
+class Activity(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+        self.last_member = None
+
+    @commands.command(aliases=['act'])
+    async def activity(self, ctx, month):
+        temp = ''''''
+        print(f'Activity gotten on {datetime.now().strftime("%B %d, %Y, %H:%M:%S")}')
+        opendoc(month[0:3])
+        process()
+        totalize(month[0:3])
+        file = open(f'text files/processed_{month[0:3]}21.txt', 'r')
+        for line in file:
+            temp = f'''{temp}{line}'''
+        resetValues()
+        embed = discord.Embed(title=f'**ACTIVITY FOR {month.upper()}**', color=0xB4C424)
+        embed.add_field(name=f'*Here is a list of the activity from {month} 1st*', value=f"{temp}")
+        await ctx.send(embed=embed)
+
+    @commands.command(aliases=['link'])
+    async def connect(self, ctx, arg=None):
+        activity_channel = client.get_channel(867303669203206194)
+        current_time = datetime.now().strftime("%B %d, %Y, %H:%M:%S")
+
+        embed1 = discord.Embed(title=f'{ctx.author} Has Connected', colour=0x50C878)
+        embed1.add_field(name='Log in document:',
+                         value=f'**CONNECT**, {current_time}, **{ctx.author}**, ||{ctx.author.id}||')
+        await ctx.send(embed=embed1)
+
+        embed = discord.Embed(title='[BETA] You Have Connected!', color=0x50C878)
+        embed.add_field(name=f'Connection marked', value=f'''
+    {ctx.author.mention}, thank you for marking down your activity!
+    Use **!dc** or **!disconnect** to mark down disconnect time!\n''')
+
+        if random.randint(1, 3) == 3 and arg is None:
+            embed.add_field(name='Tip:', value=f'''
+    You can set the bot to remind you! simply type in **2h**, **20m**, or any other time you want!''', inline=False)
+        embed.set_footer(text=f'CONNECT, {current_time}, {ctx.author}, {ctx.author.id}')
+        await ctx.send(embed=embed)
+
+        if arg is not None:
+            await ctx.send(f'I will remind you in {arg} to disconnect!')
+            if 'h' in arg:
+                arg = int(arg[0:len(arg) - 1]) * 60 * 60
+            elif 'm' in arg:
+                arg = int(arg[0:len(arg) - 1]) * 60
+            elif 's' in arg:
+                arg = int(arg[0:len(arg) - 1])
+            await asyncio.sleep(int(arg))
+            await ctx.send(f'''
+    {ctx.author.mention}, you told me to remind you {arg} seconds ago to disconnect! Make sure you did it!''')
+
+        # WriteFile = open(f'text files/activity_{current_time[0:3].lower()}21.txt', 'a')
+        # WriteFile.write(f'CONNECT, {ctx.author}, {current_time}, {ctx.author.id},\n')
+
+    @commands.command(aliases=['unlink'])
+    async def disconnect(self, ctx):
+        activity_channel = client.get_channel(867303669203206194)
+        current_time = datetime.now().strftime("%B %d, %Y, %H:%M:%S")
+
+        embed1 = discord.Embed(title=f'{ctx.author} Has Disconnected', colour=0xE97451)
+        embed1.add_field(name='Log in document:',
+                         value=f'**DISCONNECT**, {current_time}, **{ctx.author}**, ||{ctx.author.id}||')
+        await activity_channel.send(embed=embed1)
+
+        embed = discord.Embed(title='[BETA] You Have Disconnected!', color=0xE97451)
+        embed.add_field(name=f'Connection marked', value=f'''
+        {ctx.author.mention}, thank you for marking down your activity!
+        Use **!c** or **!connect** to mark down connect time!\n''')
+        embed.set_footer(text=f'DISCONNECT, {current_time}, {ctx.author}, {ctx.author.id}')
+        await ctx.send(embed=embed)
+
+        # WriteFile = open(f'text files/activity_{current_time[0:3].lower()}21.txt', 'a')
+        # WriteFile.write(f'DISCONNECT, {ctx.author}, {current_time}, {ctx.author.id},\n')
 
 
 @client.command()
@@ -44,79 +122,6 @@ async def setprefix(ctx, prefix):
         await ctx.send(f"Prefix changed to `{prefix}`")
 
 
-@client.command(aliases=['act'])
-async def activity(ctx, month):
-    temp = ''''''
-    print(f'Activity gotten on {datetime.now().strftime("%B %d, %Y, %H:%M:%S")}')
-    opendoc(month[0:3])
-    process()
-    totalize(month[0:3])
-    file = open(f'text files/processed_{month[0:3]}21.txt', 'r')
-    for line in file:
-        temp = f'''{temp}{line}'''
-    resetValues()
-    embed = discord.Embed(title= f'**ACTIVITY FOR {month.upper()}**', color=0xB4C424)
-    embed.add_field(name=f'*Here is a list of the activity from {month} 1st*', value=f"{temp}")
-    await ctx.send(embed=embed)
-
-
-@client.command(aliases=['link'])
-async def connect(ctx, arg=None):
-    activity_channel = client.get_channel(867303669203206194)
-    current_time = datetime.now().strftime("%B %d, %Y, %H:%M:%S")
-
-    embed1 = discord.Embed(title=f'{ctx.author} Has Connected', colour=0x50C878)
-    embed1.add_field(name='Log in document:', value=f'**CONNECT**, {current_time}, **{ctx.author}**, ||{ctx.author.id}||')
-    await ctx.send(embed=embed1)
-
-    embed = discord.Embed(title='[BETA] You Have Connected!', color=0x50C878)
-    embed.add_field(name=f'Connection marked', value=f'''
-{ctx.author.mention}, thank you for marking down your activity!
-Use **!dc** or **!disconnect** to mark down disconnect time!\n''')
-
-    if random.randint(1,3) == 3 and arg is None:
-        embed.add_field(name='Tip:', value=f'''
-You can set the bot to remind you! simply type in **2h**, **20m**, or any other time you want!''', inline=False)
-    embed.set_footer(text=f'CONNECT, {current_time}, {ctx.author}, {ctx.author.id}')
-    await ctx.send(embed=embed)
-
-    if arg is not None:
-        await ctx.send(f'I will remind you in {arg} to disconnect!')
-        if 'h' in arg:
-            arg = int(arg[0:len(arg)-1]) * 60 * 60
-        elif 'm' in arg:
-            arg = int(arg[0:len(arg)-1]) * 60
-        elif 's' in arg:
-            arg = int(arg[0:len(arg)-1])
-        await asyncio.sleep(int(arg))
-        await ctx.send(f'''
-{ctx.author.mention}, you told me to remind you {arg} seconds ago to disconnect! Make sure you did it!''')
-
-    #WriteFile = open(f'text files/activity_{current_time[0:3].lower()}21.txt', 'a')
-    #WriteFile.write(f'CONNECT, {ctx.author}, {current_time}, {ctx.author.id},\n')
-
-
-@client.command(aliases=['unlink'])
-async def disconnect(ctx):
-    activity_channel = client.get_channel(867303669203206194)
-    current_time = datetime.now().strftime("%B %d, %Y, %H:%M:%S")
-
-    embed1 = discord.Embed(title=f'{ctx.author} Has Disconnected', colour=0xE97451)
-    embed1.add_field(name='Log in document:',
-                     value=f'**DISCONNECT**, {current_time}, **{ctx.author}**, ||{ctx.author.id}||')
-    await activity_channel.send(embed=embed1)
-
-    embed = discord.Embed(title='[BETA] You Have Disconnected!', color=0xE97451)
-    embed.add_field(name=f'Connection marked', value=f'''
-    {ctx.author.mention}, thank you for marking down your activity!
-    Use **!c** or **!connect** to mark down connect time!\n''')
-    embed.set_footer(text=f'DISCONNECT, {current_time}, {ctx.author}, {ctx.author.id}')
-    await ctx.send(embed=embed)
-
-    #WriteFile = open(f'text files/activity_{current_time[0:3].lower()}21.txt', 'a')
-    #WriteFile.write(f'DISCONNECT, {ctx.author}, {current_time}, {ctx.author.id},\n')
-
-
 @client.event
 async def on_message(message):
     channel = client.get_channel(620441027043524618)
@@ -135,4 +140,5 @@ async def on_message(message):
     await client.process_commands(message)  # Not putting this on on_message breaks all .command()
 
 
+client.add_cog(Activity(client))
 client.run(TOKEN)
