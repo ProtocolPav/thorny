08402e896a0459e0184b97bd84169af391e6f5e2
Pavel <88209234+ProtocolPav@users.noreply.github.com>
2021-11-07 14:38:50 +0000

    Final v1.4.3

--- thorny.py
+++ thorny.py
@@ -5,11 +5,10 @@ from datetime import datetime
 import discord
 from discord.ext import commands, tasks
 
-from functions import profile_update, profile_change_months, birthday_announce
-from lottery import create_ticket, winners
+from functions import profile_update, profile_change_months, birthday_announce, tip_send
 import errors
 import json
-from modules import leaderboard, gateway, bank, playtime, profile, help, fun
+from modules import leaderboard, gateway, bank, playtime, profile, help, fun, inventory
 
 config_file = open('../thorny_data/config.json', 'r+')
 config = json.load(config_file)
@@ -24,141 +23,17 @@ elif ans == 'd':
 else:
     print('This is not a valid Token. Please run the program again.')
 
-thorny = commands.Bot(command_prefix='!')
+thorny = commands.Bot(command_prefix='!', case_insensitive=True)
 thorny.remove_command('help')
 
 
 @thorny.event
 async def on_ready():
     print(f"[ONLINE] {thorny.user}\nRunning {v}\nDate is {datetime.now()}")
-    bot_activity = discord.Activity(type=discord.ActivityType.listening,
-                                    name=f"the Evercast! | {v}")
+    bot_activity = discord.Activity(type=discord.ActivityType.watching,
+                                    name=f"for !help | {v}")
     await thorny.change_presence(activity=bot_activity)
     await profile_change_months()
-
-
-class Store(commands.Cog):
-    def __init__(self, client):
-        self.client = client
-
-    available_items = ['Empty', 'Ticket', 'Custom Role 1m']
-
-    @commands.group(aliases=['inv'], invoke_without_command=True)
-    async def inventory(self, ctx, user: discord.Member = None):
-        inventory_list = ''
-        number = 0
-        profile_json = json.load(open('../thorny_data/profiles.json', 'r'))
-        if user is None:
-            person = ctx.author
-        else:
-            person = user
-        for slot in range(1, 7):
-            number += 1
-            inventory_list = f'{inventory_list}:small_orange_diamond: ' \
-                             f'{profile_json[f"{person.id}"]["inventory"][f"slot{number}"]} **|** ' \
-                             f'{profile_json[f"{person.id}"]["inventory"][f"slot{number}_amount"]}\n'
-        await ctx.send(f"**{person}'s Inventory**\n{inventory_list}")
-
-    @inventory.command()
-    @commands.has_permissions(administrator=True)
-    async def add(self, ctx, user: discord.User = None, item=None, amnt=1):
-        profile_file = open('../thorny_data/profiles.json', 'r+')
-        profile = json.load(profile_file)
-        item_placed = False
-        slot = 0
-
-        if item.lower() == 'role' or item.lower() == 'custom':
-            while not item_placed:
-                slot += 1
-                if profile[f"{user.id}"]['inventory'][f'slot{slot}'] == 'Empty' or \
-                        profile[f"{ctx.author.id}"]['inventory'][f'slot{slot}'] == "Custom Role 1m":
-                    profile_update(user, 'Custom Role 1m', 'inventory', f'slot{slot}')
-                    amnt = profile[f"{ctx.author.id}"]['inventory'][f'slot{slot}_amount'] + int(amnt)
-                    profile_update(user, amnt, 'inventory', f'slot{slot}_amount')
-                    item_placed = True
-                    await ctx.send(f'Added item {item} to slot {slot}')
-                else:
-                    pass
-        elif item.lower() == 'ticket' or item.lower() == 'lottery':
-            while not item_placed:
-                slot += 1
-                if profile[f"{user.id}"]['inventory'][f'slot{slot}'] == 'Empty' or \
-                        profile[f"{ctx.author.id}"]['inventory'][f'slot{slot}'] == "Ticket":
-                    profile_update(user, 'Ticket', 'inventory', f'slot{slot}')
-                    amnt = profile[f"{ctx.author.id}"]['inventory'][f'slot{slot}_amount'] + int(amnt)
-                    profile_update(user, amnt, 'inventory', f'slot{slot}_amount')
-                    item_placed = True
-                    await ctx.send(f'Added item {item} to slot {slot}')
-                else:
-                    pass
-
-    @inventory.command()
-    @commands.has_permissions(administrator=True)
-    async def remove(self, ctx, user: discord.User = None, item=None):
-        profile_file = open('../thorny_data/profiles.json', 'r+')
-        profile = json.load(profile_file)
-        item_removed = False
-        slot = 0
-
-        if item.lower() == 'role' or item.lower() == 'custom':
-            while not item_removed:
-                slot += 1
-                if profile[f"{ctx.author.id}"]['inventory'][f'slot{slot}'] == "Custom Role 1m":
-                    profile_update(user, 'Empty', 'inventory', f'slot{slot}')
-                    profile_update(user, 0, 'inventory', f'slot{slot}_amount')
-                    item_removed = True
-                    await ctx.send(f'Removed item {item} from slot {slot}')
-                else:
-                    pass
-        elif item.lower() == 'ticket' or item.lower() == 'lottery':
-            while not item_removed:
-                slot += 1
-                if profile[f"{ctx.author.id}"]['inventory'][f'slot{slot}'] == "Ticket":
-                    profile_update(user, 'Empty', 'inventory', f'slot{slot}')
-                    profile_update(user, 0, 'inventory', f'slot{slot}_amount')
-                    item_removed = True
-                    await ctx.send(f'Removed item {item} from slot {slot}')
-                else:
-                    pass
-
-    @commands.group(invoke_without_command=True)
-    async def store(self, ctx):
-        await ctx.send('Items in the Store:\n'
-                       'Lottery Ticket - 10n\n'
-                       'Use !store buy item to buy an item!')
-
-    @store.command()
-    async def buy(self, ctx, item):
-        profile_file = open('../thorny_data/profiles.json', 'r+')
-        profile = json.load(profile_file)
-        item_placed = False
-        slot = 0
-
-        if item.lower() == 'ticket' or item.lower() == 'lottery':
-            if 18 <= int(datetime.now().strftime("%d")) <= 25:
-                newbal = profile[f"{ctx.author.id}"]['balance'] - 10
-                profile_update(ctx.author, newbal, 'balance')
-                while not item_placed:
-                    slot += 1
-                    if profile[f"{ctx.author.id}"]['inventory'][f'slot{slot}'] == 'Empty' or \
-                            profile[f"{ctx.author.id}"]['inventory'][f'slot{slot}'] == 'Ticket':
-                        amnt = profile[f"{ctx.author.id}"]['inventory'][f'slot{slot}_amount'] + 1
-                        profile_update(ctx.author, 'Ticket', 'inventory', f'slot{slot}')
-                        profile_update(ctx.author, amnt, 'inventory', f'slot{slot}_amount')
-                        item_placed = True
-                        ticket_number = create_ticket(ctx.author, 1)
-                        await ctx.send('Bought a ticket!')
-                        await ctx.author.send(f"Your ticket is {ticket_number}")
-                    else:
-                        pass
-            else:
-                await ctx.send(embed=errors.Shop.ticket_buy_error)
-
-    @commands.command()
-    @commands.has_permissions(administrator=True)
-    async def getwinners(self, ctx):
-        winners_list = winners(thorny)
-        await ctx.send(winners_list)
 
 
 @thorny.command()
@@ -200,7 +75,7 @@ async def on_member_join(member):
 
 thorny.add_cog(bank.Bank(thorny))
 thorny.add_cog(leaderboard.Leaderboard(thorny))
-thorny.add_cog(Store(thorny))
+thorny.add_cog(inventory.Inventory(thorny))
 thorny.add_cog(gateway.Gateway(thorny))
 thorny.add_cog(profile.Profile(thorny))
 thorny.add_cog(help.Help(thorny))
