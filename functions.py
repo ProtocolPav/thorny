1cf6be7792ff78679560aef8b4da9ada91eb195a
Pavel <88209234+ProtocolPav@users.noreply.github.com>
2022-01-27 16:19:20 +0200

    v1.6 pre-release 2

--- functions.py
+++ functions.py
@@ -214,8 +214,8 @@ def calculate_prizes(prize_list, prizes):
     nugs_reward = 0
     for item in prize_list:
         nugs_reward += item[1]
-    if prizes == prize_list[0:4]:
-        nugs_reward = nugs_reward * 5
+    if prize_list[0] != prize_list[1] != prize_list[2] != prize_list[3] and prizes[5] not in prize_list:
+        nugs_reward = nugs_reward * 2
     return nugs_reward
 
 
