71fc7afaf9dad242465d953c61b9014f76752aa7
Pavel <88209234+ProtocolPav@users.noreply.github.com>
2022-08-28 10:43:27 +0300

    Added feature #60

--- uikit/datetime_class.py
+++ uikit/datetime_class.py
@@ -6,7 +6,7 @@ class Time:
         self.time = time_object
 
     def __str__(self):
-        if type(self.time) == date:
+        if type(self.time) != timedelta:
             datetime_string = datetime.strftime(self.time, "%B %d, %Y")
             return datetime_string
         elif type(self.time) == timedelta:
