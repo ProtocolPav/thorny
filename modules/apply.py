c4b8d4caeb31d598b9b54b28342f28de4d4a8800
Pavel <88209234+ProtocolPav@users.noreply.github.com>
2022-09-23 17:06:14 +0300

    Sync local repository with remote

--- modules/apply.py
+++ modules/apply.py
@@ -8,7 +8,7 @@ class Applications(commands.Cog):
         self.client = client
 
     @commands.slash_command(description="Apply for a Project!",
-                            guild_ids=[611008530077712395, 733716450774351933])
+                            guild_ids=[733716450774351933, 611008530077712395])
     async def apply(self, ctx: discord.ApplicationContext):
         await ctx.respond(view=ProjectApplicationForm(ctx),
                           ephemeral=True)
