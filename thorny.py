b8b30ce715446586a548b774fc0aba6c2e670b80
Pavel <pbkrutov@gmail.com>
2024-06-02 23:18:28 +0300

    Money commands ported

--- thorny.py
+++ thorny.py
@@ -6,7 +6,7 @@ from discord.ext import commands, tasks
 
 import giphy_client
 from thorny_core import nexus
-import thorny_core.thorny_errors
+from thorny_core import thorny_errors
 import traceback
 import json
 import sys
